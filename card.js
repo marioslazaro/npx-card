355b12be3f9fb0f747a84b27f0ef0c8bc8376dd4
Mario Lazaro <mario-msl@hotmail.com>
2023-06-27 15:21:02 -0300

    Updating my card

--- card.js
+++ card.js
@@ -1,86 +1,55 @@
+#!/usr/bin/env node
 
-import boxen from 'boxen'
-import chalk from 'chalk'
-import inquirer  from 'inquirer'
-import clear from 'clear'
-import open  from 'open'
-
-clear()
-
-const prompt = inquirer.createPromptModule();
-
-const questions = [
-    {
-        type: "list",
-        name: "action",
-        message: "What do you want to do?",
-        choices: [
-            {
-                // Use chalk to style headers
-                name: `Toss an ${chalk.bold("email")}?`,
-                value: () => {
-                    open("mailto:mario-msl@hotmail.com");
-                    console.log("\nLooking forward to hearing your message and replying to you!\n");
-                }
-            },
-            {
-                name: "Exit",
-                value: () => {
-                    console.log("Good bye, have a nice day!\n");
-                }
-            }
-        ]
-    }
-];
+// Modules
+const boxen = require("boxen");
+const chalk = require("chalk");
+const sw = require("star-wars-quotes");
 
 const data = {
-    name: chalk.bold.green("                     Mario Lazaro"),
-    fact: chalk.hex('#B10000')('I love Open-Source!'),
-    twitter: chalk.hex('#00A1D9')("https://twitter.com/haka_dev"),
-    github: chalk.hex('#787878')("https://github.com/marioslazaro"),
-    dev: chalk.hex('#330093')("https://dev.to/marioslazaro"),
-    npx: chalk.hex('#A1AB00')("npx marioslazaro"),
+    name: chalk.red("                  Mario Lazaro"),
+    work: `${chalk.white("Software Engineer at")} ${chalk.cyan("Looking for a joob")}`,
 
-    labelFact: chalk.hex('#FF6262').bold("          Fun Fact:"),
-    labelTwitter: chalk.hex('#629DFF').bold("        Twitter:"),
-    labelGitHub: chalk.hex('#9E9E9E').bold("         GitHub:"),
-    labelDev: chalk.hex('#A959FF').bold("           Dev:"),
-    labelCard: chalk.hex('#FFF976').bold("                  Card:")
+    github: chalk.gray("https://github.com/") + chalk.blue("marioslazaro"),
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyanBright("haka_dev"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.green("marioslazaro"),
+
+    npx: chalk.red("npx marioslazaro"),
+
+    labelWork: ("         "),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
 };
 
 const me = boxen(
     [
         `${data.name}`,
         ``,
-        `${data.labelFact}  ${data.fact}`,
+        `${data.labelWork}  ${data.work}`,
         ``,
-        `${data.labelTwitter}  ${data.twitter}`,
         `${data.labelGitHub}  ${data.github}`,
-        `${data.labelDev}  ${data.dev}`,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
         ``,
         `${data.labelCard}  ${data.npx}`,
         ``,
-        `${chalk.bold(
-            "Hi there! I'm Mario, I'm a passionate JAVA stack " 
-        )}`,
-        `${chalk.bold("developer back-end from Brazil, and have a ")}`,
-        `${chalk.bold(
-            "hobby for creating creativing, cool, and solid "
-        )}`,
-        `${chalk.bold(
-            "Softwars. Toss me an email if you want to collab!"
+        
+        `${chalk.italic("    My inbox is always open, Whether you have a")}`,
+        `${chalk.italic(
+            "    question or just want talk about Star Wars! "
         )}`
     ].join("\n"),
     {
         margin: 1,
         float: 'center',
         padding: 1,
-        borderStyle: "single",
-        borderColor: "blue"
+        borderStyle: "bold",
+        borderColor: "magenta"
     }
 );
 
-// Show the boxen
 console.log(me);
 
-prompt(questions).then(answer => answer.action());
+console.log(`" ${chalk.cyanBright.italic(sw())} "`);
